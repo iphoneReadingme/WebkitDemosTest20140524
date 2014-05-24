78b1ad3b4f4cc625e48a73add76c9cded31a15eb
iphoneReadingme <readingme@126.com>
2014-05-24 11:50:36 +0800

    GHUnit 测试库 	添加单元测试代码框架 	GHUnit 测试库 	添加单元测试代码框架

---
+++ WebkitDemos20140524/WebkitDemosTests/WebkitDemosTests.m
@@ -0,0 +1,34 @@
+//
+//  WebkitDemosTests.m
+//  WebkitDemosTests
+//
+//  Created by yangfs on 14-5-24.
+//  Copyright (c) 2014年 ucweb. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WebkitDemosTests : XCTestCase
+
+@end
+
+@implementation WebkitDemosTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
